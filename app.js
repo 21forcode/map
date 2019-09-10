0f14eeb48f7e6215eecfebfb3c16e4436dea08f8
21forcode <manishlucky413@gmail.com>
2019-09-11 02:55:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $("document").ready(function(){
-    $("#send").click(function(){
+    console.log("jfdhjfd")
+	var api_data;
         var message = $("#message").val();
         $.ajax({
             url: "http://localhost:5000/api/",
@@ -7,8 +8,34 @@ $("document").ready(function(){
             contentType: "application/json",
             data: JSON.stringify({"message": message})
         }).done(function(data) {
-	var it = data.values();
-	console.log(it.next().value);
+	console.log(data[0][0]);
+	var geoJSON = {
+	    "type": "FeatureCollection",
+	    "features":[
+		{
+		    "type":"Feature",
+		    "geometry":{
+		        "type":"Polygon",
+		        "coordinates":data
+		    },
+		}
+	    ]
+	}
+var path = d3.geo.path();
+//Defaults to albersUsa Projection. You might want to set a different one
+var width = 960, height = 600;
+//Create the svg to render the polygon inside of
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+//Bind the geoJSON the path elements in the svg
+//Use the enter selection (new elements) and draw the paths using the geo path generator
+svg.selectAll("path")
+    .data(geoJSON.features)
+ .enter().append("path")
+    .attr("d",path)
+    .attr("fill");
+	 
+        console.log(data.length);
         });
-    });
 });
